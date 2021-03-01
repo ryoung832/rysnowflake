aa034b12f991e209526676221078cf894b7e2e1e
ryoung832 <79731421+ryoung832@users.noreply.github.com>
2021-03-01 11:15:13 +1100

    Delete roles.sql

--- roles.sql
+++
@@ -1,6 +0,0 @@
---Create some new security roles that represent differnet user scenarios
-create role datascientist comment = 'Data Scientists with no access to customer identifying data';
-create role analyst comment = 'Analysts with no access to customer identifying data';
-create role cust_datascientist comment = 'Data Scientists with authorised access to customer identifying data';
-create role cust_analyst comment ='Analysts with authorised access to customer identifying data';
-
